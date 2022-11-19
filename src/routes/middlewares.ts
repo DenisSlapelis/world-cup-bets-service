9781e2e0db1443dec19517925495365574f37179
Denis Slapelis <deniswesleyss@hotmail.com>
2022-11-19 15:29:18 -0300

    Fix auth param name

--- src/routes/middlewares.ts
+++ src/routes/middlewares.ts
@@ -2,7 +2,7 @@ import { userService } from '@app/users/user.service';
 import Express from 'express';
 
 export const getUserMiddleware = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    const auth = req?.headers['x-daniel-id'] || '';
+    const auth = req?.headers['user-id'] || '';
     const googleUserId = Array.isArray(auth) ? auth[0] : auth;
 
     const userId = await userService.findOneByGoogleId(googleUserId);
