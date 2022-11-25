61d6dd1093502cca5dfb153f91c0b3392fc6b639
Denis Slapelis <deniswesleyss@hotmail.com>
2022-11-25 09:34:58 -0300

    Use converted api tags to fix sync

--- src/app/integrations/world-cup.service.ts
+++ src/app/integrations/world-cup.service.ts
@@ -3,8 +3,43 @@ import axios from 'axios';
 const BASE_API_URL: string = process.env.WORLD_CUP_API || 'localhost:8000';
 
 export class WorldCupAPIervice {
+    teamTagsDict: Record<string, any>;
 
     constructor() {
+        this.teamTagsDict = {
+            ARG: 'ARG',
+            AUS: 'AUS',
+            BEL: 'BEL',
+            BRA: 'BRA',
+            CAN: 'CAN',
+            CMR: 'CAM',
+            CRC: 'CRC',
+            CRO: 'CRO',
+            DEN: 'DIN',
+            ECU: 'EQU',
+            ENG: 'ING',
+            ESP: 'ESP',
+            FRA: 'FRA',
+            GER: 'ALE',
+            GHA: 'GAN',
+            IRN: 'IRA',
+            JPN: 'JAP',
+            KOR: 'COR',
+            KSA: 'ARS',
+            MAR: 'MAR',
+            MEX: 'MEX',
+            NED: 'HOL',
+            POL: 'POL',
+            POR: 'POR',
+            QAT: 'CAT',
+            SEN: 'SEN',
+            SRB: 'SER',
+            SUI: 'SUI',
+            TUN: 'TUN',
+            URU: 'URU',
+            USA: 'EUA',
+            WAL: 'GAL',
+        };
     }
 
     matchesToday = async (options: Record<string, any>) => {
