871e966de1510d547669f291ba4e690b39f57ba1
Denis Slapelis <deniswesleyss@hotmail.com>
2023-02-18 13:42:39 -0300

    Update scripts

--- src/database/insert_matches.sql
+++ src/database/insert_matches.sql
@@ -7,79 +7,85 @@ INSERT INTO `match` (cup_id, type, match_date, team_a_id, team_b_id) VALUES (1, 
 INSERT INTO `match` (cup_id, type, match_date, team_a_id, team_b_id) VALUES (1, 'GROUP_A', '2022-11-29 15:00', (SELECT id FROM team WHERE name = 'Catar'), (SELECT id FROM team WHERE name = 'Holanda'));
 
 -- Grupo B
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 7, 'GROUP_B', '2022-11-21 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 5, 8, 'GROUP_B', '2022-11-21 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 8, 7, 'GROUP_B', '2022-11-25 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 5, 'GROUP_B', '2022-11-25 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 7, 5, 'GROUP_B', '2022-11-29 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 8, 6, 'GROUP_B', '2022-11-29 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 7, 'GROUP_B', '2022-11-21 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 5, 8, 'GROUP_B', '2022-11-21 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 8, 7, 'GROUP_B', '2022-11-25 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 5, 'GROUP_B', '2022-11-25 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 7, 5, 'GROUP_B', '2022-11-29 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 8, 6, 'GROUP_B', '2022-11-29 19:00');
 
 
 -- Grupo C
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 10, 'GROUP_C', '2022-11-22 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 11, 12, 'GROUP_C', '2022-11-22 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 12, 10, 'GROUP_C', '2022-11-26 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 11, 'GROUP_C', '2022-11-26 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 12, 9, 'GROUP_C', '2022-11-30 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 10, 11, 'GROUP_C', '2022-11-30 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 10, 'GROUP_C', '2022-11-22 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 11, 12, 'GROUP_C', '2022-11-22 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 12, 10, 'GROUP_C', '2022-11-26 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 11, 'GROUP_C', '2022-11-26 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 12, 9, 'GROUP_C', '2022-11-30 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 10, 11, 'GROUP_C', '2022-11-30 19:00');
 
 -- Grupo D
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 14, 16, 'GROUP_D', '2022-11-22 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 13, 'GROUP_D', '2022-11-22 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 16, 13, 'GROUP_D', '2022-11-26 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 14, 'GROUP_D', '2022-11-26 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 16, 15, 'GROUP_D', '2022-11-30 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 13, 14, 'GROUP_D', '2022-11-30 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 14, 16, 'GROUP_D', '2022-11-22 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 13, 'GROUP_D', '2022-11-22 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 16, 13, 'GROUP_D', '2022-11-26 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 14, 'GROUP_D', '2022-11-26 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 16, 15, 'GROUP_D', '2022-11-30 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 13, 14, 'GROUP_D', '2022-11-30 15:00');
 
 -- Grupo E
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 17, 20, 'GROUP_E', '2022-11-23 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 19, 18, 'GROUP_E', '2022-11-23 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 18, 'GROUP_E', '2022-11-27 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 19, 17, 'GROUP_E', '2022-11-27 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 19, 'GROUP_E', '2022-12-01 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 18, 17, 'GROUP_E', '2022-12-01 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 17, 20, 'GROUP_E', '2022-11-23 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 19, 18, 'GROUP_E', '2022-11-23 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 18, 'GROUP_E', '2022-11-27 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 19, 17, 'GROUP_E', '2022-11-27 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 19, 'GROUP_E', '2022-12-01 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 18, 17, 'GROUP_E', '2022-12-01 19:00');
 
 -- Grupo F
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 23, 'GROUP_F', '2022-11-23 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 21, 22, 'GROUP_F', '2022-11-23 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 21, 24, 'GROUP_F', '2022-11-27 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 22, 'GROUP_F', '2022-11-27 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 21, 'GROUP_F', '2022-12-01 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 22, 24, 'GROUP_F', '2022-12-01 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 23, 'GROUP_F', '2022-11-23 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 21, 22, 'GROUP_F', '2022-11-23 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 21, 24, 'GROUP_F', '2022-11-27 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 22, 'GROUP_F', '2022-11-27 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 21, 'GROUP_F', '2022-12-01 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 22, 24, 'GROUP_F', '2022-12-01 15:00');
 
 -- Grupo G
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 27, 26, 'GROUP_G', '2022-11-24 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 28, 'GROUP_G', '2022-11-24 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 26, 28, 'GROUP_G', '2022-11-28 10:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 27, 'GROUP_G', '2022-11-28 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 26, 25, 'GROUP_G', '2022-12-02 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 28, 27, 'GROUP_G', '2022-12-02 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 27, 26, 'GROUP_G', '2022-11-24 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 28, 'GROUP_G', '2022-11-24 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 26, 28, 'GROUP_G', '2022-11-28 10:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 27, 'GROUP_G', '2022-11-28 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 26, 25, 'GROUP_G', '2022-12-02 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 28, 27, 'GROUP_G', '2022-12-02 19:00');
 
 
 -- Grupo H
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 32, 29, 'GROUP_H', '2022-11-24 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 30, 'GROUP_H', '2022-11-24 16:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 29, 30, 'GROUP_H', '2022-11-28 13:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 32, 'GROUP_H', '2022-11-28 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 29, 31, 'GROUP_H', '2022-12-02 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 30, 32, 'GROUP_H', '2022-12-02 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 32, 29, 'GROUP_H', '2022-11-24 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 30, 'GROUP_H', '2022-11-24 16:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 29, 30, 'GROUP_H', '2022-11-28 13:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 32, 'GROUP_H', '2022-11-28 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 29, 31, 'GROUP_H', '2022-12-02 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 30, 32, 'GROUP_H', '2022-12-02 15:00');
 
 -- OITAVAS
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 3, 5, 'ROUND_OF_16', '2022-12-03 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 13, 'ROUND_OF_16', '2022-12-03 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 12, 'ROUND_OF_16', '2022-12-04 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 4, 'ROUND_OF_16', '2022-12-04 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 23, 'ROUND_OF_16', '2022-12-05 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 19, 'ROUND_OF_16', '2022-12-06 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 29, 'ROUND_OF_16', '2022-12-05 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 27, 'ROUND_OF_16', '2022-12-06 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 3, 5, 'ROUND_OF_16', '2022-12-03 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 13, 'ROUND_OF_16', '2022-12-03 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 12, 'ROUND_OF_16', '2022-12-04 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 4, 'ROUND_OF_16', '2022-12-04 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 20, 23, 'ROUND_OF_16', '2022-12-05 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 19, 'ROUND_OF_16', '2022-12-06 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 25, 29, 'ROUND_OF_16', '2022-12-05 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 31, 27, 'ROUND_OF_16', '2022-12-06 19:00');
 
 -- QUARTAS
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 25, 'QUARTER_FINAL', '2022-12-09 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 3, 9, 'QUARTER_FINAL', '2022-12-09 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 31, 'QUARTER_FINAL', '2022-12-10 15:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 15, 'QUARTER_FINAL', '2022-12-10 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 25, 'QUARTER_FINAL', '2022-12-09 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 3, 9, 'QUARTER_FINAL', '2022-12-09 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 24, 31, 'QUARTER_FINAL', '2022-12-10 15:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 6, 15, 'QUARTER_FINAL', '2022-12-10 19:00');
 
 -- SEMI
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 23, 'SEMI_FINAL', '2022-12-13 19:00');
-INSERT INTO I0ZwBCwClr.`match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 24, 'SEMI_FINAL', '2022-12-14 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 23, 'SEMI_FINAL', '2022-12-13 19:00');
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 15, 24, 'SEMI_FINAL', '2022-12-14 19:00');
+
+-- TERCEIRO LUGAR
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 23, 24, 'SEMI_FINAL', '2022-12-17 15:00');
+
+-- FINAL
+INSERT INTO `match` (cup_id, team_a_id, team_b_id, `type`, match_date) VALUES (1, 9, 15, 'SEMI_FINAL', '2022-12-18 15:00');
--- src/database/insert_teams.sql
+++ src/database/insert_teams.sql
@@ -1,47 +1,47 @@
 -- GRUPO A
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Catar', 'CAT', 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Equador', 'EQU', 'https://s.sde.globo.com/media/organizations/2019/07/15/Equador.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Holanda', 'HOL', 'https://s.sde.globo.com/media/organizations/2019/09/16/Holanda.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Senegal', 'SEN', 'https://s.sde.globo.com/media/organizations/2019/09/03/Senegal.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Catar', 'CAT', 'https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Equador', 'EQU', 'https://s.sde.globo.com/media/organizations/2019/07/15/Equador.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Holanda', 'HOL', 'https://s.sde.globo.com/media/organizations/2019/09/16/Holanda.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Senegal', 'SEN', 'https://s.sde.globo.com/media/organizations/2019/09/03/Senegal.svg');
 
 -- GRUPO B
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Estados Unidos', 'EUA', 'https://s.sde.globo.com/media/organizations/2019/07/16/Estados_Unidos.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Inglaterra', 'ING', 'https://s.sde.globo.com/media/organizations/2019/09/13/Inglaterra.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Irã', 'IRA', 'https://s.sde.globo.com/media/organizations/2019/09/01/Ir%C3%A3.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('País de Gales', 'GAL', 'https://s.sde.globo.com/media/organizations/2019/09/17/Pa%C3%ADs_de_Gales.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Estados Unidos', 'EUA', 'https://s.sde.globo.com/media/organizations/2019/07/16/Estados_Unidos.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Inglaterra', 'ING', 'https://s.sde.globo.com/media/organizations/2019/09/13/Inglaterra.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Irã', 'IRA', 'https://s.sde.globo.com/media/organizations/2019/09/01/Ir%C3%A3.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('País de Gales', 'GAL', 'https://s.sde.globo.com/media/organizations/2019/09/17/Pa%C3%ADs_de_Gales.svg');
 
 -- GRUPO C
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Argentina', 'ARG', 'https://s.sde.globo.com/media/organizations/2019/07/15/Argentina.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Arábia Saudita', 'ATA', 'https://s.sde.globo.com/media/organizations/2019/09/05/Ar%C3%A1bia_Saudita.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('México', 'MEX', 'https://s.sde.globo.com/media/organizations/2019/07/15/M%C3%A9xico.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Polônia', 'POL', 'https://s.sde.globo.com/media/organizations/2019/09/02/Polonia_Rl0m1A5.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Argentina', 'ARG', 'https://s.sde.globo.com/media/organizations/2019/07/15/Argentina.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Arábia Saudita', 'ATA', 'https://s.sde.globo.com/media/organizations/2019/09/05/Ar%C3%A1bia_Saudita.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('México', 'MEX', 'https://s.sde.globo.com/media/organizations/2019/07/15/M%C3%A9xico.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Polônia', 'POL', 'https://s.sde.globo.com/media/organizations/2019/09/02/Polonia_Rl0m1A5.svg');
 
 -- GRUPO D
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Austrália', 'AUS', 'https://s.sde.globo.com/media/organizations/2019/09/02/Australia.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Dinamarca', 'DIN', 'https://s.sde.globo.com/media/organizations/2019/09/16/Dinamarca.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('França', 'FRA', 'https://s.sde.globo.com/media/organizations/2019/09/03/Fran%C3%A7a.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Tunísia', 'TUN', 'https://s.sde.globo.com/media/organizations/2018/03/10/tunisia.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Austrália', 'AUS', 'https://s.sde.globo.com/media/organizations/2019/09/02/Australia.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Dinamarca', 'DIN', 'https://s.sde.globo.com/media/organizations/2019/09/16/Dinamarca.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('França', 'FRA', 'https://s.sde.globo.com/media/organizations/2019/09/03/Fran%C3%A7a.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Tunísia', 'TUN', 'https://s.sde.globo.com/media/organizations/2018/03/10/tunisia.svg');
 
 -- GRUPO E
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Alemanha', 'ALE', 'https://s.sde.globo.com/media/organizations/2019/09/03/Alemanha.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Costa Rica', 'CRC', 'https://s.sde.globo.com/media/organizations/2019/07/14/Costa_Rica.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Espanha', 'ESP', 'https://s.sde.globo.com/media/organizations/2019/09/03/Espanha.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Japão', 'JAP', 'https://s.sde.globo.com/media/organizations/2019/09/03/Japao.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Alemanha', 'ALE', 'https://s.sde.globo.com/media/organizations/2019/09/03/Alemanha.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Costa Rica', 'CRC', 'https://s.sde.globo.com/media/organizations/2019/07/14/Costa_Rica.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Espanha', 'ESP', 'https://s.sde.globo.com/media/organizations/2019/09/03/Espanha.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Japão', 'JAP', 'https://s.sde.globo.com/media/organizations/2019/09/03/Japao.svg');
 
 -- GRUPO F
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Bélgica', 'BEL', 'https://s.sde.globo.com/media/organizations/2019/09/15/B%C3%A9lgica.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Canadá', 'CAN', 'https://s.sde.globo.com/media/organizations/2019/07/15/Canad%C3%A1.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Croácia', 'CRO', 'https://s.sde.globo.com/media/organizations/2019/09/16/Cro%C3%A1cia.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Marrocos', 'MAR', 'https://s.sde.globo.com/media/organizations/2019/09/10/Marrocos.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Bélgica', 'BEL', 'https://s.sde.globo.com/media/organizations/2019/09/15/B%C3%A9lgica.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Canadá', 'CAN', 'https://s.sde.globo.com/media/organizations/2019/07/15/Canad%C3%A1.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Croácia', 'CRO', 'https://s.sde.globo.com/media/organizations/2019/09/16/Cro%C3%A1cia.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Marrocos', 'MAR', 'https://s.sde.globo.com/media/organizations/2019/09/10/Marrocos.svg');
 
 -- GRUPO G
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Brasil', 'BRA', 'https://s.sde.globo.com/media/organizations/2019/07/16/Brasil_rgYHF6Z.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Camarões', 'CAM', 'https://s.sde.globo.com/media/organizations/2019/09/10/Camar%C3%B5es.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Suíça', 'SUI', 'https://s.sde.globo.com/media/organizations/2019/09/15/Su%C3%AD%C3%A7a.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Sérvia', 'SER', 'https://s.sde.globo.com/media/organizations/2019/09/02/Servia.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Brasil', 'BRA', 'https://s.sde.globo.com/media/organizations/2019/07/16/Brasil_rgYHF6Z.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Camarões', 'CAM', 'https://s.sde.globo.com/media/organizations/2019/09/10/Camar%C3%B5es.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Suíça', 'SUI', 'https://s.sde.globo.com/media/organizations/2019/09/15/Su%C3%AD%C3%A7a.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Sérvia', 'SER', 'https://s.sde.globo.com/media/organizations/2019/09/02/Servia.svg');
 
 -- GRUPO H
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Coreia do Sul', 'COR', 'https://s.sde.globo.com/media/organizations/2019/09/01/Coreia_do_Sul.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Gana', 'GAN', 'https://s.sde.globo.com/media/organizations/2019/09/08/Gana.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Portugal', 'POR', 'https://s.sde.globo.com/media/organizations/2019/09/17/Portugal.svg');
-INSERT INTO I0ZwBCwClr.team (name, tag, avatar) VALUES ('Uruguai', 'URU', 'https://s.sde.globo.com/media/organizations/2019/07/16/Uruguai.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Coreia do Sul', 'COR', 'https://s.sde.globo.com/media/organizations/2019/09/01/Coreia_do_Sul.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Gana', 'GAN', 'https://s.sde.globo.com/media/organizations/2019/09/08/Gana.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Portugal', 'POR', 'https://s.sde.globo.com/media/organizations/2019/09/17/Portugal.svg');
+INSERT INTO team (name, tag, avatar) VALUES ('Uruguai', 'URU', 'https://s.sde.globo.com/media/organizations/2019/07/16/Uruguai.svg');
